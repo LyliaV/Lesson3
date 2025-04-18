1906e69d6675b5c5143eababa1582d3773890a0f
LyliaV <edav17@bk.ru>
2025-04-18 20:02:33 +0300

    string. Задание 1

--- src/string/task1/ReplaceSpace.java
+++ src/string/task1/ReplaceSpace.java
@@ -1,4 +1,18 @@
 package string.task1;
 
 public class ReplaceSpace {
+
+    public static void main(String[] args) {
+
+// Напишите программу, которая заменяет все пробелы в строке на символ подчеркивания (_)
+        replace_Space("В абапе строки проще");
+    }
+
+    private static void replace_Space(String text) {
+
+        System.out.println("Старая строка: " + text);
+
+        System.out.println("Новая строка: " + text.replace(" ", "_"));
+
+    }
 }
